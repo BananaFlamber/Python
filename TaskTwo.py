ea4db76d87f004a2ce0a9ba474d33717356090ed
BananaFlamber <KorolevSteel@inbox.ru>
2022-10-09 15:01:31 +0300

    all done

--- TaskTwo.py
+++ TaskTwo.py
@@ -1,11 +1,13 @@
 print("\033c")
+try:
+    X = int(input("Введите число X: "))
+    Y = int(input("Введите число Y: "))
+    Z = int(input("Введите число Z: "))
 
-X = int(input("Введите число X: "))
-Y = int(input("Введите число Y: "))
-Z = int(input("Введите число Z: "))
-
-for X in range(2):
+    for X in range(2):
         for Y in range(2):
             for Z in range(2):
                 print(not (X or Y or Z) == (not X and not Y and not Z))
                 print(X, Y, Z)
+except:
+    print("Введены некоректные данные!")
